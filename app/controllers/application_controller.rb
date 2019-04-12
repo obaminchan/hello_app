99da7437e10f4d9879ec1554c828063bf3a43b7e
EC2 Default User <ec2-user@ip-172-31-35-127.us-east-2.compute.internal>
2019-04-12 03:21:17 +0000

    2019/04/12

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  def goodbye
-   render html: "goodbye, world!"
+  def hello
+   render html: "hola, mundo!"
   end
 end
